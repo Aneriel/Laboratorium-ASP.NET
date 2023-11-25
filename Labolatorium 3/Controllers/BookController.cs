b2a1fa50db9ac43709692c82becd9c175c75dd4e
Aneriel <bartlomiej.kaczmarsk@microsoft.wsei.edu.pl>
2023-11-25 16:11:39 +0100

    praca nad pobieraniem

--- Labolatorium 3/Controllers/BookController.cs
+++ Labolatorium 3/Controllers/BookController.cs
@@ -1,14 +1,15 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Labolatorium_3.Models;
+using System.Reflection;
 
 namespace Labolatorium_3.Controllers
 {
     public class BookController : Controller
     {
-        static Dictionary<int, Book> _contacts = new Dictionary<int, Book>();
+        static Dictionary<int, Book> _book = new Dictionary<int, Book>();
         public IActionResult Index()
         {
-            return View();
+            return View(_book);
         }
         [HttpGet]
         public IActionResult Create()
@@ -16,12 +17,15 @@ namespace Labolatorium_3.Controllers
             return View();
         }
         [HttpPost]
-        public IActionResult Create(Book book) {
+        public IActionResult Create(Book model) {
         if(ModelState.IsValid)
             {
-                return RedirectToAction("Index");
+                int id = _book.Keys.Count != 0 ? _book.Keys.Max() : 0;
+                model.Id = id + 1;
+                _book.Add(model.Id, model);
+
             }
-        return View(book);
+            return View(model);
         }
     }
 }
