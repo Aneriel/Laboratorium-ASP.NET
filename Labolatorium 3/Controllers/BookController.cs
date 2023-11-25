4427fe4763856b5fa1d825c3f7ed27d259022960
Aneriel <bartlomiej.kaczmarsk@microsoft.wsei.edu.pl>
2023-11-25 15:59:15 +0100

    poprawa niezapisanych commitów

---
+++ Labolatorium 3/Controllers/BookController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Labolatorium_3.Models;
+
+namespace Labolatorium_3.Controllers
+{
+    public class BookController : Controller
+    {
+        static Dictionary<int, Book> _contacts = new Dictionary<int, Book>();
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Book book) {
+        if(ModelState.IsValid)
+            {
+                return RedirectToAction("Index");
+            }
+        return View(book);
+        }
+    }
+}
