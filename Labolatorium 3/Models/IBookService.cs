09abede1f07c6afb73b9365f304894e0dfef7cbb
Aneriel <bartlomiej.kaczmarsk@microsoft.wsei.edu.pl>
2023-11-25 15:00:27 +0100

    123

---
+++ Labolatorium 3/Models/IBookService.cs
@@ -0,0 +1,13 @@
+﻿using Laboratorium_3.Models;
+
+namespace Labolatorium_3.Models
+{
+    public interface IBookService
+    {
+        void Add(Contact book);
+        void Delete(int id);
+        void Update(Contact book);
+        List<Contact> FindAll();
+        Contact? FindById(int id);
+    }
+}
